4d00e513645384eb1d5abda5bad4c64e232106e5
Riley Slayden <slaydenriley@gmail.com>
2020-01-23 16:18:36 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,12 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    self.songs.first
+  end
+
   def get_genre_of_first_song
-    self.song.genres.first
+    get_first_song.genre
   end
 
   def song_count
